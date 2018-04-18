1f6461f98d9129469aee4cb5c4fa79623ef72384
Kattig Jane <KattigJane@Kattig-E-Jane.local>
2018-04-18 14:51:18 +0700

    node express for production

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.info('Server is listening on port: ' + PORT + '!');
+    }
+});
